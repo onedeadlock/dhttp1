d4fbab305c3b8d1e5a432811b8eccb8bd0907a1e
onedeadlock <saviourme33@gmail.com>
2026-09-12 10:32:56 +0100

    update

--- src/common/bits.hpp
+++ src/common/bits.hpp
@@ -1,104 +1,102 @@
 #pragma once
 #include "common.hpp"
-#if HAVE_VIST_C_
-#   include <immintrin.h>
+#if __HAVE_MSVC__
+#   include <intrin.h>
+#elif __GNUC__
+#include <x86intrin.h>
 #endif
 
+#ifdef __HAVE_MSVC__
+#endif
 namespace dhttp::common::bits
 {
-    template <typename T=u64_t>
+    template <typename T>
     inline T lsb(T x)
     {
-        static_assert(std::is_integral_v<T> and !std::is_signed_v<T>);
         return x & -x;
     }
 
-    template <typename T=u64_t>
-    inline T trim(T x)
+    template <typename T>
+    make_flat inline T trim(T x)
     {
-        static_assert(std::is_integral_v<T> and !std::is_signed_v<T>);
+        if constexpr (__HAVE_MSVC__ or __HAVE_GNUC__)
+            return __andn_u64(x, x << 1);
         return x & ~(x << 1);
     }
 
-    template <typename T=u64_t>
-    inline T trim_u(T x)
+    template <typename T>
+    make_flat inline T trim_u(T x)
     {
-        static_assert(std::is_integral_v<T> and !std::is_signed_v<T>);
+        if constexpr (__HAVE_MSVC__ or __HAVE_GNUC__)
+             return __andn_u64(U64(x), U64(x) >> 1);
         return x & ~(x >> 1);
     }
 
-    template <typename T=u64_t>
-    inline T tzmask(T x)
+    template <typename T>
+    make_flat inline T tzmask(T x)
     {
-        static_assert(std::is_integral_v<T> and !std::is_signed_v<T>);
+        if constexpr (__HAVE_MSVC__ or __HAVE_GNUC__)
+             return __andn_u64(U64(x) - 1, U64(x));
         return ~x & (x - 1);
     }
 
-    template <typename T=u64_t>
+    template <typename T>
     inline T blsmask(T x)
     {
-        static_assert(std::is_integral_v<T> and !std::is_signed_v<T>);
+        if constexpr (__HAVE_MSVC__ or __HAVE_GNUC__)
+             return __blsmsk_u64(U64(x));
         return x ^ (x - 1);
     }
 
-    template <typename T=u64_t>
+    template <typename T>
     inline T blsr(T x)
     {
-        static_assert(std::is_integral_v<T> and !std::is_signed_v<T>);
+        if constexpr (__HAVE_MSVC__ or __HAVE_GNUC__)
+             return __blsr_u64(U64(x));
         return x & (x - 1);
     }
 
-    template <typename T=u64_t>
+    template <typename T>
     inline T blsfill(T x)
     {
-        static_assert(std::is_integral_v<T> and !std::is_signed_v<T>);
+        if constexpr (__HAVE_MSVC__ or __HAVE_GNUC__)
+             return __blsfill_u64(U64(x));
         return x | (x - 1);
     }
 
-    template <typename T=u64_t>
+    template <typename T>
     inline T xlsfill(T x)
     {
-        static_assert(std::is_integral_v<T> and !std::is_signed_v<T>);
-        // xlsfiil is not an actual instruction, but it does the opposite of blsfill
+        if constexpr (__HAVE_MSVC__ or __HAVE_GNUC__)
+             return ~__blsfill_u64(U64(x));
         return x ^ -x;
     }
 
-    template <typename T=u64_t>
+    template <typename T>
     inline T tzcnt(T x)
     {
-        static_assert(std::is_integral_v<T> and !std::is_signed_v<T>);
-        if constexpr (sizeof (T) == 8)
+        if constexpr (sizeof (T) == 32)
         {
-#if __GNUC__
-        return __builtin_ctzll(x);
-#elif __MSVC__
-        T vx;
-        _BitScanReverse(&vx, x);
-        return vx;
+#if defined(_tzcnt_u32) || defined(__HAVE_MSVC__)
+            return _tzcnt_u32(x);
+#elif __HAVE_GNUC__
+        return __builtin_ctz(x);
 #else
-            x |= x >> 1;
-            x |= x >> 2;
-            x |= x >> 4;
-            x |= x >> 8;
-            x |= x >> 16;
-            x |= x >> 32;
-            return constant::DeBruijn64_seq[(x * constant::DeBruijn64_const) >> 58];
+            x |= x >> 1; x |= x >> 2;
+            x |= x >> 4; x |= x >> 8;
+            return constant::DeBruijn32_seq[((x | x >> 16) * constant::DeBruijn32_const) >> 24];
 #endif
         }
-#if __GNUC__
-        return __builtin_ctz(static_cast<u32_t>(x));
-#elif __MSVC__
-        u32_t vx;
-        _BitScanReverse32(&vx, static_cast<u32_t>(x));
-        return vx;
+
+#if defined(_tzcnt_u64) || defined(__HAVE_MSVC__)
+            return _tzcnt_u64(x);
+#elif __HAVE_GNUC__
+        return __builtin_ctzll(x);
 #else
-            u32_t v = static_cast<u32_t>(v);
-            v |= v >> 1;
-            v |= v >> 2;
-            v |= v >> 4;
-            v |= v >> 8;
-            v |= v >> 16;
-            return constant::DeBruijn32_seq[(v * constant::DeBruijn32_const) >> 24]; // TODO: DB table 32
+            x |= x >> 1;  x |= x >> 2;
+            x |= x >> 4;  x |= x >> 8;
+            x |= x >> 16; x |= x >> 32;
+            return constant::DeBruijn64_seq[(x * constant::DeBruijn64_const) >> 58];
 #endif
     }
 }
